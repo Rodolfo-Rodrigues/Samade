20a1fbadc6c968cbd181f3836956fb7c87bad381
Rodolfo-Rodrigues <50324906+Rodolfo-Rodrigues@users.noreply.github.com>
2020-03-24 01:00:56 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,5 @@
+var menu = document.querySelector('.menu');
+var showMenu = document.querySelector('.fa-bars');
+showMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+})
